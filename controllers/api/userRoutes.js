cdcb760ca619364281ba9b88b43940e7cc11ae31
Ethan Buchanan <ethan@buchananville.com>
2022-02-23 21:00:17 -0800

    added route /api/users/register

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -48,6 +48,52 @@ router.post('/login', async (req, res) => {
   }
 });
 
+router.post('/register', async (req, res) => {
+  try {
+    const userNameTest = await User.findOne({ where: { name: req.body.name } });
+
+    if (userNameTest) {
+      res
+        .status(400)
+        .json({ message: 'Username already in use' });
+      return;
+    }
+    
+    const emailNameTest = await User.findOne({ where: { email: req.body.email } });
+
+    if (emailNameTest) {
+      res
+        .status(400)
+        .json({ message: 'Email already has an account associated with it' });
+      return;
+    }
+
+     User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password
+    },
+    {
+      individualHooks: true,
+      returning: true,
+    });
+
+    const userData = await (await User.findOne({ where: { name: req.body.name } })).toJSON();
+
+
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      
+      res.json({ user: userData, message: 'New account \"' + userData.name + "\" was created" });
+    });
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 router.post('/logout', (req, res) => {
   if (req.session.logged_in) {
     req.session.destroy(() => {
