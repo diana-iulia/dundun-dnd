b2da1fd9c926814dddea1f232a3daca7b275df7f
Nick Cooper <92492572+NCooper00@users.noreply.github.com>
2022-02-26 10:49:28 -0800

    Created create html pages for the campaigns and cards

---
+++ public/js/npc-create-page.js
@@ -0,0 +1,51 @@
+const alignOverlay = document.querySelector('.alignOverlay');
+const alignIcon = document.querySelectorAll('.alignIcon');
+const selectedAlign = document.querySelector('.selectedAlignment');
+const emptyAlign = document.querySelector('.userNPCAlign');
+const alignWord = document.querySelector('.alignWord');
+
+for (i=0; i<alignIcon.length; i++) {
+    alignIcon[i].addEventListener('click', (event) => {
+        var src = event.currentTarget.getAttribute('src');
+        var alt = event.currentTarget.getAttribute('alt');
+        console.log(event.currentTarget)
+        selectedAlign.setAttribute('aria-enabled', true);
+        emptyAlign.setAttribute('data-visible', false);
+        selectedAlign.setAttribute('src', src);
+        alignOverlay.setAttribute('aria-enabled', false)
+        alignWord.textContent = alt;
+    })
+};
+
+selectedAlign.addEventListener('click', () => {
+    alignOverlay.setAttribute('aria-enabled', true);
+});
+
+emptyAlign.addEventListener('click', () => {
+    alignOverlay.setAttribute('aria-enabled', true);
+});
+
+
+
+const emptyIcon = document.querySelector('.userNPCIcon');
+const selectedCharacter = document.querySelector('.selectedCharacter');
+const npcIconOverlay = document.querySelector('.npcIconOverlay');
+const characterIcon = document.querySelectorAll('.characterIcon');
+
+for (i=0; i<characterIcon.length; i++) {
+    characterIcon[i].addEventListener('click', (event) => {
+        var src = event.currentTarget.getAttribute('src');
+        selectedCharacter.setAttribute('src', src);
+        selectedCharacter.setAttribute('aria-enabled', true);
+        npcIconOverlay.setAttribute('aria-enabled', false);
+        emptyIcon.setAttribute('data-visible', false);
+    });
+};
+
+emptyIcon.addEventListener('click', () => {
+    npcIconOverlay.setAttribute('aria-enabled', true);
+});
+
+selectedCharacter.addEventListener('click', () => {
+    npcIconOverlay.setAttribute('aria-enabled', true);
+});
